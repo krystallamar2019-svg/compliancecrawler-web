44e672f4b23fd409f3676d325d81039c83573df7
krystallamar2019-svg <krystallamar2019@gmail.com>
2026-09-14 21:05:04 -0600

    Refresh BrandedAlign PWA icon cache

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
-const CACHE_NAME='brandedalign-v1';
-const APP_SHELL=['/','/manifest.webmanifest','/brandedalign-app-icon.svg'];
+const CACHE_NAME='brandedalign-v2';
+const APP_SHELL=[
+  '/',
+  '/manifest.webmanifest',
+  '/brandedalign-app-icon-180.png',
+  '/brandedalign-app-icon-192.png',
+  '/brandedalign-app-icon-512.png'
+];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).catch(()=>null));
@@ -33,12 +39,12 @@ self.addEventListener('fetch',event=>{
   }
 
   event.respondWith(
-    caches.match(request).then(cached=>cached||fetch(request).then(response=>{
+    fetch(request).then(response=>{
       if(response&&response.ok){
         const copy=response.clone();
         caches.open(CACHE_NAME).then(cache=>cache.put(request,copy)).catch(()=>{});
       }
       return response;
-    }))
+    }).catch(()=>caches.match(request))
   );
 });
